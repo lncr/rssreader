1f79f413a8a75626f5ac5258d98bdbf4f5adcdf5
Bektur Soltobaev <bektur_soltobaev@epam.com>
2022-09-12 18:01:57 +0600

    Refactored for using feedparser

--- rss_reader.py
+++ rss_reader.py
@@ -1,33 +1,16 @@
 import requests
+import feedparser
+import argparse
 
 
-res = requests.get('https://news.yahoo.com/rss/')
+parser = argparse.ArgumentParser()
 
-content = res.text
+parser.add_argument('source', type=str,
+                    help='Receives RSS url and prints results in human-readable format')
 
+args = parser.parse_args()
 
-def collect_news(content):
-    '''
-    Generates list filled with elements of 
-    <item>...</item>
-    '''
-    found = content.find('<item>')
+url = args.source
+d = feedparser.parse(url)
 
-    content = content[found:]
-
-    news = []
-
-    while True:
-
-        found = content.find('</item>')
-        if found < 0:
-            break
-        new = content[:found]
-        content = content[found+7:]
-        news.append(new)
-
-    return news
-
-
-if __name__ == '__main__':
-    print(collect_news())
+print(d['entries'][0])
